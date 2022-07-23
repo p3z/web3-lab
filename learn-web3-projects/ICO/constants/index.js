e6ee8fd4cfb487d836914d54ce512c2a0ae7a539
Linux PC (Shay) <shay@pottleweb.solutions>
2022-07-23 23:40:51 +0100

    Create app for ICO

--- learn-web3-projects/ICO/constants/index.js
+++ learn-web3-projects/ICO/constants/index.js
@@ -1,7 +1,6 @@
 // Address of the NFT Contract that you deployed
 const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xD1804F8aD61f775397e70B8fC3Fffb62B662D575";
 
-// Address this ICO contracted was deployed to
-// Crypto Devs Token Contract Address: 0x137AaA93487Dd639B8929290595e05038de5401B
+
 
 module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
