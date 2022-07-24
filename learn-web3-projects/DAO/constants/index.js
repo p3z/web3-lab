e6109286a07d614891eed52a1d0529f0929734cd
Shay <chenille.pottle@yahoo.co.uk>
2022-07-24 09:46:18 +0100

    DAO tutorial

---
+++ learn-web3-projects/DAO/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xD1804F8aD61f775397e70B8fC3Fffb62B662D575";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
